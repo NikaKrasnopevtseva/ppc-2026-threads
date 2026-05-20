b8864da1a02e230203eef1de2f54d42f81e46e1d
NikaKrasnopevtseva <Krasnopevtseva_veronika@mail.ru>
2026-05-20 21:13:00 +0000

    fix

--- tasks/krasnopevtseva_v_hoare_batcher_sort/all/include/ops_all.hpp
+++ tasks/krasnopevtseva_v_hoare_batcher_sort/all/include/ops_all.hpp
@@ -19,15 +19,31 @@ class KrasnopevtsevaVHoareBatcherSortALL : public BaseTask {
   bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
+  struct Chunk {
+    int *ptr;
+    int size;
+    int left;
+    int right;
+  };
+
   static int Partition(std::vector<int> &arr, int first, int last);
-  static void QuickSort(std::vector<int> &arr, int first, int last);
   static void InsertionSort(std::vector<int> &arr, int first, int last);
+  static void QuickSort(std::vector<int> &arr, int first, int last);
   static void BatcherMergeBlocksStep(int *left_pointer, int &left_size, int *right_pointer, int &right_size);
   static void BatcherMerge(int thread_input_size, std::vector<int *> &pointers, std::vector<int> &sizes,
                            int par_if_greater);
   static void ParallelSortChunksOpenMP(std::vector<int> &res, int n, int numthreads);
-  static void MPIDistributeData(std::vector<int> &data, int rank, int size);
-  static void MPIGatherResults(std::vector<int> &local_data, int rank, int size, int global_n);
-  static void MPIMergeResults(std::vector<int> &global_data, const std::vector<std::vector<int>> &all_data);
+  static void MergeSortedVectors(std::vector<int> &result, const std::vector<int> &other);
+
+  static void ComputeDistribution(int n, int size, std::vector<int> &send_counts, std::vector<int> &displs);
+  static void ScatterData(const std::vector<int> &input, std::vector<int> &local_data, int rank, int size);
+  static void SortLocalData(std::vector<int> &local_data);
+  static void GatherData(std::vector<int> &local_data, std::vector<int> &global_data, int rank, int size, int n);
+  static void FinalMerge(std::vector<int> &global_data, const std::vector<int> &send_counts,
+                         const std::vector<int> &displs, int size);
+
+  int rank_ = 0;
+  int proc_size_ = 1;
+  bool mpi_initialized_ = false;
 };
 }  // namespace krasnopevtseva_v_hoare_batcher_sort
--- tasks/krasnopevtseva_v_hoare_batcher_sort/all/src/ops_all.cpp
+++ tasks/krasnopevtseva_v_hoare_batcher_sort/all/src/ops_all.cpp
@@ -21,241 +21,11 @@ KrasnopevtsevaVHoareBatcherSortALL::KrasnopevtsevaVHoareBatcherSortALL(const InT
 }
 
 bool KrasnopevtsevaVHoareBatcherSortALL::ValidationImpl() {
-  const auto &input = GetInput();
-  return !input.empty();
+  return !GetInput().empty();
 }
 
 bool KrasnopevtsevaVHoareBatcherSortALL::PreProcessingImpl() {
   GetOutput() = std::vector<int>();
-  return true;
-}
-
-void KrasnopevtsevaVHoareBatcherSortALL::ParallelSortChunksOpenMP(std::vector<int> &res, int n, int numthreads) {
-  if (n <= 0) {
-    return;
-  }
-
-  numthreads = std::min(n, numthreads);
-  if (numthreads <= 0) {
-    numthreads = 1;
-  }
-
-  int thread_input_size = n / numthreads;
-  int thread_input_remainder_size = n % numthreads;
-
-  std::vector<int *> pointers(numthreads);
-  std::vector<int> sizes(numthreads);
-
-  for (int i = 0; i < numthreads; ++i) {
-    std::ptrdiff_t offset = static_cast<std::ptrdiff_t>(i) * static_cast<std::ptrdiff_t>(thread_input_size);
-    pointers[i] = res.data() + offset;
-    sizes[i] = thread_input_size;
-  }
-  sizes[sizes.size() - 1] += thread_input_remainder_size;
-
-#pragma omp parallel for default(none) shared(res, pointers, sizes, numthreads)
-  for (int i = 0; i < numthreads; ++i) {
-    int left = static_cast<int>(pointers[i] - res.data());
-    int right = left + sizes[i] - 1;
-    if (left <= right) {
-      QuickSort(res, left, right);
-    }
-  }
-
-  BatcherMerge(thread_input_size, pointers, sizes, 32);
-}
-
-void KrasnopevtsevaVHoareBatcherSortALL::MPIDistributeData(std::vector<int> &data, int rank, int size) {
-  int n = static_cast<int>(data.size());
-  int local_n = n / size;
-  int remainder = n % size;
-
-  std::vector<int> send_counts(size);
-  std::vector<int> displs(size);
-
-  for (int i = 0; i < size; ++i) {
-    send_counts[i] = local_n + (i < remainder ? 1 : 0);
-    displs[i] = (i == 0) ? 0 : displs[i - 1] + send_counts[i - 1];
-  }
-
-  int my_local_n = send_counts[rank];
-  std::vector<int> local_data(my_local_n);
-
-  MPI_Scatterv(data.data(), send_counts.data(), displs.data(), MPI_INT, local_data.data(), my_local_n, MPI_INT, 0,
-               MPI_COMM_WORLD);
-
-  data = std::move(local_data);
-}
-
-void KrasnopevtsevaVHoareBatcherSortALL::MPIGatherResults(std::vector<int> &local_data, int rank, int size,
-                                                          int global_n) {
-  std::vector<int> recv_counts(size);
-  std::vector<int> displs(size);
-
-  int local_n = static_cast<int>(local_data.size());
-
-  MPI_Allgather(&local_n, 1, MPI_INT, recv_counts.data(), 1, MPI_INT, MPI_COMM_WORLD);
-
-  displs[0] = 0;
-  for (int i = 1; i < size; ++i) {
-    displs[i] = displs[i - 1] + recv_counts[i - 1];
-  }
-
-  std::vector<int> global_data(global_n);
-  MPI_Gatherv(local_data.data(), local_n, MPI_INT, global_data.data(), recv_counts.data(), displs.data(), MPI_INT, 0,
-              MPI_COMM_WORLD);
-
-  if (rank == 0) {
-    local_data = std::move(global_data);
-  }
-}
-
-void KrasnopevtsevaVHoareBatcherSortALL::MPIMergeResults(std::vector<int> &global_data,
-                                                         const std::vector<std::vector<int>> &all_data) {
-  if (all_data.empty()) {
-    return;
-  }
-
-  std::vector<int> result = all_data[0];
-
-  for (size_t i = 1; i < all_data.size(); ++i) {
-    std::vector<int> merged;
-    merged.reserve(result.size() + all_data[i].size());
-
-    size_t i1 = 0, i2 = 0;
-    while (i1 < result.size() && i2 < all_data[i].size()) {
-      if (result[i1] <= all_data[i][i2]) {
-        merged.push_back(result[i1++]);
-      } else {
-        merged.push_back(all_data[i][i2++]);
-      }
-    }
-    while (i1 < result.size()) {
-      merged.push_back(result[i1++]);
-    }
-    while (i2 < all_data[i].size()) {
-      merged.push_back(all_data[i][i2++]);
-    }
-
-    result = std::move(merged);
-  }
-
-  global_data = std::move(result);
-}
-
-bool KrasnopevtsevaVHoareBatcherSortALL::RunImpl() {
-  int rank = 0, size = 1;
-  int initialized = 0;
-  MPI_Initialized(&initialized);
-
-  if (!initialized) {
-    MPI_Init(nullptr, nullptr);
-  }
-
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  const auto &input = GetInput();
-  std::size_t global_n = input.size();
-
-  if (global_n <= 1) {
-    if (rank == 0) {
-      GetOutput() = input;
-    }
-    if (!initialized) {
-      MPI_Finalize();
-    }
-    return true;
-  }
-
-  int n = static_cast<int>(global_n);
-  int local_n = n / size;
-  int remainder = n % size;
-
-  std::vector<int> send_counts(size);
-  std::vector<int> displs(size);
-
-  for (int i = 0; i < size; ++i) {
-    send_counts[i] = local_n + (i < remainder ? 1 : 0);
-    displs[i] = (i == 0) ? 0 : displs[i - 1] + send_counts[i - 1];
-  }
-
-  int my_n = send_counts[rank];
-  std::vector<int> local_data(my_n);
-
-  if (rank == 0) {
-    std::vector<int> temp_input = input;
-    MPI_Scatterv(temp_input.data(), send_counts.data(), displs.data(), MPI_INT, local_data.data(), my_n, MPI_INT, 0,
-                 MPI_COMM_WORLD);
-  } else {
-    MPI_Scatterv(nullptr, nullptr, nullptr, MPI_INT, local_data.data(), my_n, MPI_INT, 0, MPI_COMM_WORLD);
-  }
-
-  if (my_n > 0) {
-    int numthreads = omp_get_max_threads();
-    numthreads = std::min(my_n, numthreads);
-
-    if (my_n < 10000) {
-      QuickSort(local_data, 0, my_n - 1);
-    } else {
-      ParallelSortChunksOpenMP(local_data, my_n, numthreads);
-    }
-  }
-
-  std::vector<int> global_data;
-
-  if (rank == 0 && n > 0) {
-    global_data.resize(static_cast<size_t>(n));
-  }
-
-  MPI_Gatherv(local_data.data(), my_n, MPI_INT, global_data.data(), send_counts.data(), displs.data(), MPI_INT, 0,
-              MPI_COMM_WORLD);
-
-  if (rank == 0 && n > 0) {
-    std::vector<std::vector<int>> all_parts;
-    all_parts.reserve(static_cast<size_t>(size));
-
-    for (int i = 0; i < size; ++i) {
-      if (send_counts[i] > 0 && displs[i] + send_counts[i] <= n) {
-        all_parts.emplace_back(global_data.begin() + displs[i], global_data.begin() + displs[i] + send_counts[i]);
-      }
-    }
-
-    if (!all_parts.empty()) {
-      std::vector<int> result = std::move(all_parts[0]);
-
-      for (size_t i = 1; i < all_parts.size(); ++i) {
-        std::vector<int> merged;
-        merged.reserve(result.size() + all_parts[i].size());
-
-        size_t i1 = 0, i2 = 0;
-        while (i1 < result.size() && i2 < all_parts[i].size()) {
-          if (result[i1] <= all_parts[i][i2]) {
-            merged.push_back(result[i1++]);
-          } else {
-            merged.push_back(all_parts[i][i2++]);
-          }
-        }
-        while (i1 < result.size()) {
-          merged.push_back(result[i1++]);
-        }
-        while (i2 < all_parts[i].size()) {
-          merged.push_back(all_parts[i][i2++]);
-        }
-
-        result = std::move(merged);
-      }
-
-      GetOutput() = std::move(result);
-    } else {
-      GetOutput() = std::vector<int>();
-    }
-  }
-
-  if (!initialized) {
-    MPI_Finalize();
-  }
-
   return true;
 }
 
@@ -344,6 +114,169 @@ void KrasnopevtsevaVHoareBatcherSortALL::BatcherMerge(int thread_input_size, std
       BatcherMergeBlocksStep(pointers[idx1], sizes[idx1], pointers[idx2], sizes[idx2]);
     }
   }
+}
+
+void KrasnopevtsevaVHoareBatcherSortALL::ParallelSortChunksOpenMP(std::vector<int> &res, int n, int numthreads) {
+  if (n <= 0) {
+    return;
+  }
+
+  numthreads = std::min(n, numthreads);
+  if (numthreads <= 0) {
+    numthreads = 1;
+  }
+
+  int thread_input_size = n / numthreads;
+  int thread_input_remainder_size = n % numthreads;
+
+  std::vector<int *> pointers(numthreads);
+  std::vector<int> sizes(numthreads);
+
+  for (int i = 0; i < numthreads; ++i) {
+    std::ptrdiff_t offset = static_cast<std::ptrdiff_t>(i) * static_cast<std::ptrdiff_t>(thread_input_size);
+    pointers[i] = res.data() + offset;
+    sizes[i] = thread_input_size;
+  }
+  sizes.back() += thread_input_remainder_size;
+
+#pragma omp parallel for default(none) shared(res, pointers, sizes, numthreads)
+  for (int i = 0; i < numthreads; ++i) {
+    int left = static_cast<int>(pointers[i] - res.data());
+    int right = left + sizes[i] - 1;
+    if (left < right) {
+      QuickSort(res, left, right);
+    }
+  }
+
+  BatcherMerge(thread_input_size, pointers, sizes, 32);
+}
+
+void KrasnopevtsevaVHoareBatcherSortALL::SortLocalData(std::vector<int> &data) {
+  int n = static_cast<int>(data.size());
+  if (n <= 0) {
+    return;
+  }
+
+  int numthreads = omp_get_max_threads();
+  numthreads = std::min(n, numthreads);
+
+  if (n < 10000) {
+    QuickSort(data, 0, n - 1);
+  } else {
+    ParallelSortChunksOpenMP(data, n, numthreads);
+  }
+}
+
+bool KrasnopevtsevaVHoareBatcherSortALL::RunImpl() {
+  int rank = 0;
+  int proc_size = 1;
+  int initialized = 0;
+
+  MPI_Initialized(&initialized);
+
+  if (!initialized) {
+    MPI_Init(nullptr, nullptr);
+  }
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &proc_size);
+
+  const auto &input = GetInput();
+  int n = static_cast<int>(input.size());
+
+  if (n <= 1) {
+    if (rank == 0) {
+      GetOutput().resize(input.size());
+      std::copy(input.begin(), input.end(), GetOutput().begin());
+    }
+    if (!initialized) {
+      MPI_Finalize();
+    }
+    return true;
+  }
+
+  int local_n = n / proc_size;
+  int remainder = n % proc_size;
+  std::vector<int> send_counts(proc_size);
+  std::vector<int> displs(proc_size);
+
+  for (int i = 0; i < proc_size; ++i) {
+    send_counts[i] = local_n + (i < remainder ? 1 : 0);
+    displs[i] = (i == 0) ? 0 : displs[i - 1] + send_counts[i - 1];
+  }
+
+  int my_n = send_counts[rank];
+  std::vector<int> local_data(my_n);
+
+  if (rank == 0) {
+    std::vector<int> temp = input;
+    MPI_Scatterv(temp.data(), send_counts.data(), displs.data(), MPI_INT, local_data.data(), my_n, MPI_INT, 0,
+                 MPI_COMM_WORLD);
+  } else {
+    MPI_Scatterv(nullptr, nullptr, nullptr, MPI_INT, local_data.data(), my_n, MPI_INT, 0, MPI_COMM_WORLD);
+  }
+
+  SortLocalData(local_data);
+
+  std::vector<int> global_data;
+  if (rank == 0) {
+    global_data.resize(static_cast<size_t>(n));
+  }
+
+  MPI_Gatherv(local_data.data(), my_n, MPI_INT, global_data.data(), send_counts.data(), displs.data(), MPI_INT, 0,
+              MPI_COMM_WORLD);
+
+  if (rank == 0) {
+    std::vector<std::vector<int>> parts;
+    parts.reserve(static_cast<size_t>(proc_size));
+
+    for (int i = 0; i < proc_size; ++i) {
+      int cnt = send_counts[i];
+      int start = displs[i];
+      if (cnt > 0 && start + cnt <= n) {
+        parts.emplace_back(global_data.begin() + start, global_data.begin() + start + cnt);
+      }
+    }
+
+    if (!parts.empty()) {
+      std::vector<int> result = std::move(parts[0]);
+      for (size_t i = 1; i < parts.size(); ++i) {
+        std::vector<int> merged;
+        merged.reserve(result.size() + parts[i].size());
+
+        size_t i1 = 0;
+        size_t i2 = 0;
+        while (i1 < result.size() && i2 < parts[i].size()) {
+          if (result[i1] <= parts[i][i2]) {
+            merged.push_back(result[i1]);
+            ++i1;
+          } else {
+            merged.push_back(parts[i][i2]);
+            ++i2;
+          }
+        }
+        while (i1 < result.size()) {
+          merged.push_back(result[i1]);
+          ++i1;
+        }
+        while (i2 < parts[i].size()) {
+          merged.push_back(parts[i][i2]);
+          ++i2;
+        }
+        result = std::move(merged);
+      }
+      global_data = std::move(result);
+    }
+
+    GetOutput().resize(global_data.size());
+    std::copy(global_data.begin(), global_data.end(), GetOutput().begin());
+  }
+
+  if (!initialized) {
+    MPI_Finalize();
+  }
+
+  return true;
 }
 
 }  // namespace krasnopevtseva_v_hoare_batcher_sort
